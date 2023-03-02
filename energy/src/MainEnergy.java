c7ea35cc2154215553f0c8d07084ca5703dea347
benakli <william.benakli.4.2@gmail.com>
2023-03-02 16:48:44 +0100

    Changes by david

--- energy/src/MainEnergy.java
+++ energy/src/MainEnergy.java
@@ -8,7 +8,10 @@ import vue.FenetreJFrame;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainEnergy {
@@ -60,32 +63,31 @@ public class MainEnergy {
 
     private static void parseLine(String line, int positionLine, Level level) throws ParseException {
         String[] arrayLine = line.split(" ");
+
+        TuileComposant currComposant = null;
+        ArrayList<Integer> listConnexion = new ArrayList<Integer>();
+
         for (String s : arrayLine) {
-           if ((s.charAt(0) >= 65 && s.charAt(0) <= 90) || s.charAt(0) == 46) {
+            if ((s.charAt(0) >= 65 && s.charAt(0) <= 90) || s.charAt(0) == 46) {
                 //TODO: Contruire un nouvel objet
-               System.out.println(s + "avant switch(s)");
-               switch (s) {
-                   case "." -> level.addTuile((positionLine - 1), new TuileVide());
-                   case "S" -> {
-                       System.out.println("ENERGY");
-                       level.addTuile((positionLine - 1), new TuileCarre(TuileComposant.ENERGY));
-                   }
-                   case "L" -> {
-                       System.out.println("LAMP");
-                       level.addTuile((positionLine - 1), new TuileCarre(TuileComposant.LIGHT));
-                   }
-                   case "W" -> {
-                       System.out.println("WIFI");
-                       level.addTuile((positionLine - 1), new TuileCarre(TuileComposant.WIFI));
-                   }
-                   default -> {
-                       System.out.println(s + " croisé");
-                       throw new ParseException("Erreur à la ligne", positionLine);
-                   }
+                System.out.println(s + "avant switch(s)");
+
+                if(currComposant!=null){
+                    level.addTuile((positionLine - 1), new TuileCarre(currComposant, new ArrayList<>(listConnexion)));
+                    listConnexion.clear();
+                }
+
+                switch (s) {
+                    case "." -> currComposant = TuileComposant.AUCUN;
+                    case "S" -> currComposant = TuileComposant.ENERGY;
+                    case "L" -> currComposant = TuileComposant.LIGHT;
+                    case "W" -> currComposant = TuileComposant.WIFI;
+                    default -> throw new ParseException("Erreur à la ligne", positionLine);
                }
 
             } else if (s.charAt(0) >= 48 && s.charAt(0) <= 57) {
                 //TODO: Ajouter des liens à un objet
+                listConnexion.add(Integer.parseInt(s));
             } else throw new ParseException("Erreur à la ligne", positionLine);
         }
 
